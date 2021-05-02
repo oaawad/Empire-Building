c02f3d96c578061c2e91dfa0042542456e46da5e
unknown <o.ashraaf@yahoo.com>
2021-05-02 18:13:30 +0200

    add Exceptions classes

---
+++ src/exceptions/EmpireException.java
@@ -0,0 +1,11 @@
+package exceptions;
+
+public abstract class EmpireException extends Exception{
+	// A subclass of Exception. Class representing a generic exception that can occur during the game play. These exceptions arise from any invalid action that is performed.
+	public EmpireException() {
+		super();
+	}
+	public EmpireException(String s) {
+		super(s);
+	}
+}
